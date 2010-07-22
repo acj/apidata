27caf285b768aeb50f32d9ebda1bea6e192c26aa
Adam Jensen <acj@maclaurin.local>
2010-07-21 21:59:33 -0400

    Added Senate Committee on Armed Services

--- committees/update_committees.py
+++ committees/update_committees.py
@@ -1604,7 +1604,23 @@ def subcomm_senate_appropriations():
     
     return member_string
 
-tasks = [comm_agriculture, subcomm_agriculture, comm_appropriations, subcomm_appropriations, comm_armedservices, subcomm_armedservices, comm_budget, comm_edlabor, subcomm_edlabor, comm_energycommerce, subcomm_energycommerce, comm_financialservices, subcomm_financialservices, comm_foreignaffairs, subcomm_foreignaffairs, comm_energygw, comm_permanentintel, comm_rules, comm_veterans, comm_waysandmeans, subcomm_waysandmeans, comm_transportation, subcomm_transportation, comm_smallbusiness, subcomm_smallbusiness, comm_science, subcomm_hsc, comm_cha, comm_natres, comm_oversight, subcomm_oversight, comm_homelandsecurity, comm_help, comm_foreign, subcomm_foreign, comm_senate_finance, subcomm_senate_finance, comm_senate_ethics, comm_senate_energy, subcomm_senate_energy, comm_senate_ag, comm_senate_appropriations, subcomm_senate_appropriations]
+def comm_senate_armedservices():
+    members = ['"Senate Committee on Armed Services"', '"SSAS"']
+    page = urllib2.urlopen('http://armed-services.senate.gov/members.htm')
+    soup = BeautifulSoup(page)
+    member_containers = soup.findAll('p')
+    for m in member_containers:
+        for a in m.findAll('a'):
+            name = str(a.contents[0])
+            paren_pos = name.find(' (')
+            if paren_pos == -1:
+                continue
+            name = name[:paren_pos]
+            members.append('"' + name + '"')
+
+    return ', '.join(members) + '\n'
+
+tasks = [comm_agriculture, subcomm_agriculture, comm_appropriations, subcomm_appropriations, comm_armedservices, subcomm_armedservices, comm_budget, comm_edlabor, subcomm_edlabor, comm_energycommerce, subcomm_energycommerce, comm_financialservices, subcomm_financialservices, comm_foreignaffairs, subcomm_foreignaffairs, comm_energygw, comm_permanentintel, comm_rules, comm_veterans, comm_waysandmeans, subcomm_waysandmeans, comm_transportation, subcomm_transportation, comm_smallbusiness, subcomm_smallbusiness, comm_science, subcomm_hsc, comm_cha, comm_natres, comm_oversight, subcomm_oversight, comm_homelandsecurity, comm_help, comm_foreign, subcomm_foreign, comm_senate_finance, subcomm_senate_finance, comm_senate_ethics, comm_senate_energy, subcomm_senate_energy, comm_senate_ag, comm_senate_appropriations, subcomm_senate_appropriations, comm_senate_armedservices]
 
 for t in tasks:
     print t(),

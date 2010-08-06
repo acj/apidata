1d6efb117ebbeacd1a604cae645f378022594ab3
Adam Jensen <acj@maclaurin.local>
2010-08-05 21:46:13 -0400

    Added Senate Committee on Veterans' Affairs.

--- committees/update_committees.py
+++ committees/update_committees.py
@@ -1847,8 +1847,21 @@ def comm_senate_sbc():
 
     return ', '.join(members) + '\n'
 
+def comm_senate_veterans():
+    members = ['"Senate Committee on Veterans\' Affairs"', '"SSVA"']
+    page = urllib2.urlopen('http://veterans.senate.gov/committee-members.cfm')
+    soup = BeautifulSoup(page)
+    member_containers = soup.findAll('font', color='#006600')
+    for m in member_containers:
+        name = m.contents[0]
+        commapos = name.find(', ')
+        name = name[:commapos]
+        members.append('"' + name.strip() + '"')
 
-tasks = [comm_agriculture, subcomm_agriculture, comm_appropriations, subcomm_appropriations, comm_armedservices, subcomm_armedservices, comm_budget, comm_edlabor, subcomm_edlabor, comm_energycommerce, subcomm_energycommerce, comm_financialservices, subcomm_financialservices, comm_foreignaffairs, subcomm_foreignaffairs, comm_energygw, comm_permanentintel, comm_rules, comm_veterans, comm_waysandmeans, subcomm_waysandmeans, comm_transportation, subcomm_transportation, comm_smallbusiness, subcomm_smallbusiness, comm_science, subcomm_hsc, comm_cha, comm_natres, comm_oversight, subcomm_oversight, comm_homelandsecurity, comm_help, comm_foreign, subcomm_foreign, comm_senate_finance, subcomm_senate_finance, comm_senate_ethics, comm_senate_energy, subcomm_senate_energy, comm_senate_ag, comm_senate_appropriations, subcomm_senate_appropriations, comm_senate_armedservices, comm_senate_banking, subcomm_senate_banking, comm_senate_commerce, subcomm_senate_commerce, comm_senate_budget, comm_senate_judiciary, subcomm_senate_judiciary, comm_senate_rules, comm_senate_sbc]
+    return ', '.join(members) + '\n'
+
+
+tasks = [comm_agriculture, subcomm_agriculture, comm_appropriations, subcomm_appropriations, comm_armedservices, subcomm_armedservices, comm_budget, comm_edlabor, subcomm_edlabor, comm_energycommerce, subcomm_energycommerce, comm_financialservices, subcomm_financialservices, comm_foreignaffairs, subcomm_foreignaffairs, comm_energygw, comm_permanentintel, comm_rules, comm_veterans, comm_waysandmeans, subcomm_waysandmeans, comm_transportation, subcomm_transportation, comm_smallbusiness, subcomm_smallbusiness, comm_science, subcomm_hsc, comm_cha, comm_natres, comm_oversight, subcomm_oversight, comm_homelandsecurity, comm_help, comm_foreign, subcomm_foreign, comm_senate_finance, subcomm_senate_finance, comm_senate_ethics, comm_senate_energy, subcomm_senate_energy, comm_senate_ag, comm_senate_appropriations, subcomm_senate_appropriations, comm_senate_armedservices, comm_senate_banking, subcomm_senate_banking, comm_senate_commerce, subcomm_senate_commerce, comm_senate_budget, comm_senate_judiciary, subcomm_senate_judiciary, comm_senate_rules, comm_senate_sbc, comm_senate_veterans]
 
 for t in tasks:
     print t(),

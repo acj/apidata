1ba63e6aacb73cc6718b5ee15e1e79b6e3cf1918
Adam Jensen <acj@maclaurin.local>
2010-08-05 22:25:05 -0400

    Added Joint Committee on Taxation, and Joint Committee on the Library.

--- committees/update_committees.py
+++ committees/update_committees.py
@@ -1904,7 +1904,33 @@ def comm_joint_econ():
 
     return ', '.join(members) + '\n'
 
-tasks = [comm_agriculture, subcomm_agriculture, comm_appropriations, subcomm_appropriations, comm_armedservices, subcomm_armedservices, comm_budget, comm_edlabor, subcomm_edlabor, comm_energycommerce, subcomm_energycommerce, comm_financialservices, subcomm_financialservices, comm_foreignaffairs, subcomm_foreignaffairs, comm_energygw, comm_permanentintel, comm_rules, comm_veterans, comm_waysandmeans, subcomm_waysandmeans, comm_transportation, subcomm_transportation, comm_smallbusiness, subcomm_smallbusiness, comm_science, subcomm_hsc, comm_cha, comm_natres, comm_oversight, subcomm_oversight, comm_homelandsecurity, comm_help, comm_foreign, subcomm_foreign, comm_senate_finance, subcomm_senate_finance, comm_senate_ethics, comm_senate_energy, subcomm_senate_energy, comm_senate_ag, comm_senate_appropriations, subcomm_senate_appropriations, comm_senate_armedservices, comm_senate_banking, subcomm_senate_banking, comm_senate_commerce, subcomm_senate_commerce, comm_senate_budget, comm_senate_judiciary, subcomm_senate_judiciary, comm_senate_rules, comm_senate_sbc, comm_senate_veterans, comm_senate_aging, comm_senate_intel, comm_joint_econ]
+def comm_joint_library():
+    members = ['"Joint Economic Committee"', '"JSEC"']
+    page = urllib2.urlopen('http://www.senate.gov/general/committee_membership/committee_memberships_JSLC.htm')
+    soup = BeautifulSoup(page)
+    member_containers = soup.findAll('pub_last')
+    for m_lname in member_containers:
+        lname = m_lname.contents[0]
+        m_fname = m_lname.nextSibling.nextSibling
+        fname = m_fname.contents[0]
+        members.append('"' + fname + ' ' + lname + '"')
+
+    return ', '.join(members) + '\n'
+
+def comm_joint_taxation():
+    members = ['"Joint Economic on Taxation"', '"JSTX"']
+    page = urllib2.urlopen('http://www.jct.gov/')
+    soup = BeautifulSoup(page)
+    member_containers = soup.findAll('td', align='center', valign='top')
+    for m in member_containers:
+        for a in m.findAll('a'):
+            name = str(a.contents[0])
+            name = name.replace('<br />', '')
+            members.append('"' + name + '"')
+
+    return ', '.join(members) + '\n'
+
+tasks = [comm_agriculture, subcomm_agriculture, comm_appropriations, subcomm_appropriations, comm_armedservices, subcomm_armedservices, comm_budget, comm_edlabor, subcomm_edlabor, comm_energycommerce, subcomm_energycommerce, comm_financialservices, subcomm_financialservices, comm_foreignaffairs, subcomm_foreignaffairs, comm_energygw, comm_permanentintel, comm_rules, comm_veterans, comm_waysandmeans, subcomm_waysandmeans, comm_transportation, subcomm_transportation, comm_smallbusiness, subcomm_smallbusiness, comm_science, subcomm_hsc, comm_cha, comm_natres, comm_oversight, subcomm_oversight, comm_homelandsecurity, comm_help, comm_foreign, subcomm_foreign, comm_senate_finance, subcomm_senate_finance, comm_senate_ethics, comm_senate_energy, subcomm_senate_energy, comm_senate_ag, comm_senate_appropriations, subcomm_senate_appropriations, comm_senate_armedservices, comm_senate_banking, subcomm_senate_banking, comm_senate_commerce, subcomm_senate_commerce, comm_senate_budget, comm_senate_judiciary, subcomm_senate_judiciary, comm_senate_rules, comm_senate_sbc, comm_senate_veterans, comm_senate_aging, comm_senate_intel, comm_joint_econ, comm_joint_library, comm_joint_taxation]
 
 for t in tasks:
     print t(),
